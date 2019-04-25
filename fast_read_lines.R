8b061a0ac58fb0733c50bca905a3ec9f5e29beb1
Edward Davis <hello@edwdavis.com>
2019-04-24 19:00:20 -0700

    add fast_read_lines.R

---
+++ fast_read_lines.R
@@ -0,0 +1,6 @@
+fast_read_lines <- function(filename) {
+  size <- file.info(filename)$size
+  buf <- readChar(filename, size, useBytes = TRUE)
+  line <- strsplit(buf, "\n", fixed = TRUE, useBytes = TRUE)[[1]]
+  str_replace_all(line, "[\r]", "")
+}
